d114621656a6c4c1a3bd846165c14183cfe18fb1
Capucinoxx <bendertermebc@gmail.com>
2021-12-26 15:59:49 -0500

    [feat] - 8kyu: multiply the number

---
+++ javascript/8kyu/multiply_the_number.js
@@ -0,0 +1,9 @@
+// +-------------------------------------------------------------------------+
+// | Multiply the number                                                     |
+// |                                                                         |
+// | Date:    25 décembre 2021                                               |
+// | Author:  Benjamin Courchesne (Capucinoxx)                               |
+// | Link:    https://www.codewars.com/kata/5708f682c69b48047b000e07         |
+// +-------------------------------------------------------------------------+
+
+cont multiply = (n) => n * 5 ** (Math.abs(n) + "").length
